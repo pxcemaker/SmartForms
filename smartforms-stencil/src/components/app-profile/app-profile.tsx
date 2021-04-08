3f863ad4e861b04d904a03b0b7bb5f03cec52569
TabeaWoehr <tabi.woehr@gmail.com>
2021-04-08 23:29:01 +0200

    put in html-basic-structure and some css to work with it

--- smartforms-stencil/src/components/app-profile/app-profile.tsx
+++ smartforms-stencil/src/components/app-profile/app-profile.tsx
@@ -14,13 +14,12 @@ export class AppProfile {
     if (name) {
       return name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase();
     }
-    
 
     return '';
   }
   componentWillLoad() {
     return fetch('/assets/blubb.json')
-      .then((response) => response.json())
+      .then(response => response.json())
       .then(data => {
         this.content = data;
         console.log(this.content);
@@ -30,12 +29,29 @@ export class AppProfile {
     if (this.match && this.match.params.name) {
       return (
         <div class="app-profile">
-          {this.content.map((umfrage) =>
-        <div>
-          <div>{umfrage.title}</div>
-          <div>{umfrage.body}</div>
-        </div>
-      )}
+          <div id="profilesection">
+            <div class="contentcontainer">
+              <div id="profilepicture"></div>
+              <div id="profiletext">
+                <p id="username">Username</p>
+                <p id="usermail">usermail@e-mail.de</p>
+                <button id="">Profil bearbeiten</button>
+              </div>
+            </div>
+          </div>
+          <div class="contentcontainer">
+            <div id="overview">
+              <h1>
+                Umfragen<br></br>Übersicht
+              </h1>
+              {this.content.map(umfrage => (
+                <div class="onesurveyelement">
+                  <div>{umfrage.title}</div>
+                  <div>{umfrage.body}</div>
+                </div>
+              ))}
+            </div>
+          </div>
           <p>Hello! My name is {this.normalize(this.match.params.name)}. My name was passed in through a route param!</p>
         </div>
       );
@@ -48,5 +64,4 @@ class Umfrage {
   id: number;
   title: string;
   body: string;
-
 }
