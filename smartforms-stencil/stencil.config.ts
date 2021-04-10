1dc78f541f1a5afcdbbb6782b06ecc75602729e2
TabeaWoehr <tabi.woehr@gmail.com>
2021-04-10 11:17:32 +0200

    intergrated bootstrap and deleted css-files

--- smartforms-stencil/stencil.config.ts
+++ smartforms-stencil/stencil.config.ts
@@ -1,10 +1,12 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
+  globalStyle: 'src/global/app.scss',
   globalScript: 'src/global/app.ts',
+  plugins: [sass()],
   taskQueue: 'async',
   outputTargets: [
     {
