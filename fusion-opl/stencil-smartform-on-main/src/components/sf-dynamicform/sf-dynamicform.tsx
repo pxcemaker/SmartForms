5b87240966ef7c02008d63329fcea0c75c2efda8
MeliV <melissa.vela@web.de>
2021-04-29 08:40:38 +0200

    radioButton

--- fusion-opl/stencil-smartform-on-main/src/components/sf-dynamicform/sf-dynamicform.tsx
+++ fusion-opl/stencil-smartform-on-main/src/components/sf-dynamicform/sf-dynamicform.tsx
@@ -74,19 +74,9 @@ export class SfDynamicform {
     if (this.radio == 'checkbox') {
       return this.checkboxMap.map(checkboxdef => <sf-checkbox value={checkboxdef.value}></sf-checkbox>);
     } else if (this.radio == 'radio') {
-<<<<<<< HEAD
-      return (
-        <div class="checkbox-kasten">
-          {this.radioMap.map(radiodef => (
-            <sf-radio value={radiodef.value} radio-Id={} radio-Name={}></sf-radio>
-          ))}
-        </div>
-      );
-=======
-      return this.radioMap.map(radiodef => <sf-radio value={radiodef.value}></sf-radio>);
+      return this.radioMap.map(radiodef => <sf-radio value={radiodef.value} radio-Id={} radio-Name={}></sf-radio>);
     } else if (this.radio == 'rtx') {
       return <sf-text-area></sf-text-area>;
->>>>>>> eba6b3cbf2fe0322a2d9a5e5f1f423bde736250e
     }
   }
 
