1a9ec5c0ed868485e4e1c525d754209b05675afd
TabeaWoehr <tabi.woehr@gmail.com>
2021-04-29 23:08:32 +0200

    added editformarea to umfrage.php

--- fusion-opl/umfrage.php
+++ fusion-opl/umfrage.php
@@ -10,7 +10,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-   
+
     <link rel="stylesheet" href="assets/css/umfrage.css">
 
     <!-- von Stencil Projekt -->
@@ -27,86 +27,88 @@
         <!-- Navbar Start -->
         <?php require("nav.inc.php"); ?>
         <!-- Navbar End -->
-   
 
-     <!-- Hero Area Start -->
-     <div id="hero-area" class="hero-area-bg">
-        <div class="container">      
-          <div class="row">
-            <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
-              <!-- <div class="contents"> -->
-                <h2 class="head-title">   <br>   <br>UMFRAGE<br>ERSTELLEN.<br>   </h2>
-                <!-- <p>Umfrage-Tool zur Messung von Marktforschung, <br>Kundenzufriedenheit, Mitarbeiterbeurteilung und <br>vieles mehr.</p> -->
-                <!-- <div class="header-button">
+
+        <!-- Hero Area Start -->
+        <div id="hero-area" class="hero-area-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
+                        <!-- <div class="contents"> -->
+                        <h2 class="head-title"> <br> <br>UMFRAGE<br>ERSTELLEN.<br> </h2>
+                        <!-- <p>Umfrage-Tool zur Messung von Marktforschung, <br>Kundenzufriedenheit, Mitarbeiterbeurteilung und <br>vieles mehr.</p> -->
+                        <!-- <div class="header-button">
                   <a href="#" class="btn btn-border" id="start-btn">LOSLEGEN</i></a>
                 </div> -->
-              <!-- </div> -->
+                        <!-- </div> -->
+                    </div>
+                    <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
+                        <div class="intro-img">
+                            <img class="img-fluid" src="../img/smartformBild.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
-              <div class="intro-img">
-                <img class="img-fluid" src="../img/smartformBild.jpg" alt="">
-              </div>            
-            </div>
-          </div> 
-        </div> 
-      </div>
-      <!-- Hero Area End -->
-    
+        </div>
+        <!-- Hero Area End -->
+
     </header>
-      <p>Checkboxen:</p>
-      <div class="checkbox-container">
-          <div class="checkbox-kasten">
-             <div class="checkbox-content">
-                  <input type="checkbox" id="a" name="a"
-                      checked>
-                  <label for="a">Option A</label>
-              </div> 
-          </div>
+    <p>Checkboxen:</p>
+    <div class="checkbox-container">
+        <div class="checkbox-kasten">
+            <div class="checkbox-content">
+                <input type="checkbox" id="a" name="a" checked>
+                <label for="a">Option A</label>
+            </div>
+        </div>
 
         <div class="checkbox-kasten">
             <div class="checkbox-content">
-                 <input type="checkbox" id="b" name="b"
-                     checked>
-                 <label for="b">Option B</label>
-             </div> 
-         </div>
+                <input type="checkbox" id="b" name="b" checked>
+                <label for="b">Option B</label>
+            </div>
+        </div>
+
+        <sf-editformarea></sf-editformarea>
     </div>
 
-      <!-- Footer Section Start -->
-      <footer id="footer" class="footer-area section-padding">
+    <!-- Footer Section Start -->
+    <footer id="footer" class="footer-area section-padding">
         <div class="container">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
-                <div class="widget">
-                  <h3 class="footer-logo"><img src="assets/img/smartform-logo-grey.png" alt=""></h3>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+                        <div class="widget">
+                            <h3 class="footer-logo"><img src="assets/img/smartform-logo-grey.png" alt=""></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                        <div class="textwidget">
+                            <p><a href="impressum.html">IMPRESSUM</a> / <a href="datenschutz.html">DATENSCHUTZ</a></p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="textwidget">
-                  <p><a href="impressum.html">IMPRESSUM</a> / <a href="datenschutz.html">DATENSCHUTZ</a></p>
-                </div>
-              </div>
             </div>
-          </div>  
-        </div> 
+        </div>
         <div id="copyright">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="copyright-content">
-                  <p>Copyright © 2020 <a rel="nofollow" href="https://uideck.com">UIdeck</a> All Right Reserved</p>
-                  <p>https://onepagelove.com/fusion-lite</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="copyright-content">
+                            <p>Copyright © 2020 <a rel="nofollow" href="https://uideck.com">UIdeck</a> All Right
+                                Reserved</p>
+                            <p>https://onepagelove.com/fusion-lite</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>   
-      </footer> 
-      <!-- Footer Section End -->
-    </body>
-    </html>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+</body>
+
+</html>
