a4e7f5be0d99ccd83de5964f11a9f1af41eab79b
MeliV <melissa.vela@web.de>
2021-05-03 21:34:40 +0200

    Abschlussseite-Teilnahme added

---
+++ fusion-opl/abschlussseite-teilnahme.php
@@ -0,0 +1,18 @@
+<?php require("head.inc.php"); ?>
+<?php require("nav.inc.php"); ?>
+<div style="margin-top: 8rem;"></div>
+<section>
+        <div class="container">
+            <div class="row">
+                <h2 id="sliderTitel"> Du hast erfolgreich<br>
+                    an der Umfrage
+                    <br>teilgenommen
+                </h2>
+                <div id="linkBereich"></div>
+                <img src="assets/img/ipadinhand.png" id="slider">
+            </div>
+        </div>
+        </div>
+
+    </section>
+<?php include("footer.inc.php"); ?>
