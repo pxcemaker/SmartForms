4fcefb3b5e95aa0e6b8f95c28a12de1c666c5962
selina-ne <81676593+selina-ne@users.noreply.github.com>
2021-05-04 12:31:57 +0200

    call to action

--- fusion-opl/HowTo.php
+++ fusion-opl/HowTo.php
@@ -124,6 +124,22 @@
         </div>
         </div>
     </section>
+    <!-- Call To Action Section Start -->
+    
+   
+    <section class="section-padding eigeneSec">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-xs-12" data-wow-delay="0.3s">
+                    <div class="eigeneSec">
+                        <h1 class="eigeneSec">Steigern Sie Kundenzufriedenheit. Erfassen Sie schnell und einfach Kundenfeedback.</h1>
+                        <a href="umfrage.php" class="btn btn-common mt-3">loslegen</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+     <!--ende slider section-->
     <!-- Footer Section Start -->
     <?php include("footer.inc.php"); ?>
 
