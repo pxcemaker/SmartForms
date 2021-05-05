e7e8561e610cc09b08b0d2ee63538161493e2b08
TabeaWoehr <tabi.woehr@gmail.com>
2021-05-05 11:43:17 +0200

    deleted old files

--- fusion-opl/test.php
+++
@@ -1,6 +0,0 @@
-<?php require("head.inc.php"); ?>
-<?php require("nav.inc.php"); ?>
-<div style="margin-top: 15rem;"></div>
-<sf-editformarea></sf-editformarea>
-<div style="margin-bottom: 15rem;"></div>
-<?php include("footer.inc.php"); ?>
