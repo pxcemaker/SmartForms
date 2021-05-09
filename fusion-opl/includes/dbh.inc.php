20f8bd8154bc165f04755e942c8e7219ec6bb204
Paula <paulakarlin.pk@gmail.com>
2021-05-09 21:42:37 +0200

    Verbindung zum live SQL Server

---
+++ fusion-opl/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername ="db1573.mydbserver.com";
+$dbUsername ="p585307";
+$dbPassword ="k-8q.c4Aolvrki";
+$dbName ="usr_p585307_1";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
